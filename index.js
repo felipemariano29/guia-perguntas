927168510848c02087af932e9f1762ea5ec645aa
Felipe Mariano <86112326+felipemariano29@users.noreply.github.com>
2024-04-23 11:48:13 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar");
+});
+
+app.listen(port, () => console.log(`Servidor iniciado na porta ${port}`));
