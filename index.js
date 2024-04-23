409f2d88990bbb19c9a6eaeda7cea2ef13c53ea4
Felipe Mariano <86112326+felipemariano29@users.noreply.github.com>
2024-04-23 12:05:43 -0300

    Body-parser setup

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 8080;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -11,6 +16,13 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req, res) => {
   res.render("perguntar");
+});
+
+app.post("/salvar", (req, res) => {
+  const titulo = req.body.titulo;
+  const descricao = req.body.descricao;
+
+  res.send(`O título da pergunta é '${titulo}' e a descrição é '${descricao}'`);
 });
 
 app.listen(port, () => console.log(`Servidor iniciado na porta ${port}`));
